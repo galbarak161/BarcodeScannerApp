f90fda78d0e4b8d19e9e220b73c99cea71d91a85
galbarak161 <galbarak161@gmail.com>
2022-07-09 01:58:26 +0300

    Add the ability to copy content to clipboard

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.barcodescanner;
 
+import android.app.ListActivity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends ListActivity {
 
     // Initialize variable
-    Button btScan;
+    Button btScan, btCopy;
+    ArrayList<String> listItems = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +32,57 @@ public class MainActivity extends AppCompatActivity {
 
         // Assign variable
         btScan = findViewById(R.id.bt_scan);
-        btScan.setOnClickListener(view -> {
-            // Initialize intent integrator
-            IntentIntegrator intentIntegrator = new IntentIntegrator(
-                    MainActivity.this
-            );
+        btScan.setOnClickListener(view -> ScannerButtonClickEvent());
 
-            // Set prompt text and beep
-            intentIntegrator.setPrompt("For flash use volume up key");
-            intentIntegrator.setBeepEnabled(true);
+        btCopy = findViewById(R.id.bt_copyList);
+        btCopy.setEnabled(false);
+        btCopy.setOnClickListener(view -> CopyButtonClickEvent());
 
-            // Locked orientation
-            intentIntegrator.setOrientationLocked(true);
+        // Link adapter to ListView
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
 
-            // Set capture activity and initialize scan
-            intentIntegrator.setCaptureActivity(Capture.class);
-            intentIntegrator.initiateScan();
+    private void CopyButtonClickEvent() {
+        String result = String.join(" , ", listItems);
+
+        // Save the list content to clipboard
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", result);
+        clipboard.setPrimaryClip(clip);
+
+        // Create alert for the user
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                MainActivity.this
+        );
+        builder.setTitle("Copy to clipboard");
+        builder.setMessage(String.format(Locale.getDefault(), "Copied %d elements to the clipboard", listItems.size()));
+        builder.setPositiveButton("OK", (dialogInterface, i) -> {
+            // Dismiss dialog
+            dialogInterface.dismiss();
         });
+
+        builder.show();
+    }
+
+    private void ScannerButtonClickEvent() {
+        // Initialize intent integrator
+        IntentIntegrator intentIntegrator = new IntentIntegrator(
+                MainActivity.this
+        );
+
+        // Set prompt text and beep
+        intentIntegrator.setPrompt("For flash use volume up key");
+        intentIntegrator.setBeepEnabled(true);
+
+        // Locked orientation
+        intentIntegrator.setOrientationLocked(true);
+
+        // Set capture activity and initialize scan
+        intentIntegrator.setCaptureActivity(Capture.class);
+        intentIntegrator.initiateScan();
     }
 
     @Override
@@ -53,17 +95,12 @@ public class MainActivity extends AppCompatActivity {
         );
 
         if (result.getContents() != null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(
-                    MainActivity.this
-            );
-            builder.setTitle("Results");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", (dialogInterface, i) -> {
-                // Dismiss dialog
-                dialogInterface.dismiss();
-            });
+            adapter.add(result.getContents());
+            btCopy.setEnabled(true);
 
-            builder.show();
+            // Update the button content
+            btScan.setText(R.string.button_secondClick);
+
         } else {
             Toast.makeText(getApplicationContext(), "Oops.. Try again", Toast.LENGTH_SHORT).show();
         }
